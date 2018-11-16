9485dd514e0b49a46f50373eb4376504d7d7bad2
Ariel-R-Cepeda <43122482+Ariel-R-Cepeda@users.noreply.github.com>
2018-11-15 23:04:11 -0500

    My update
    Fixed flaws
    added an easter egg
    added hints

--- main.py
+++ main.py
@@ -13,16 +13,32 @@ while True:
     lives = 3
     values = 10
     level = 1
+    #for a small easter egg
+    hint_count=0
     # Number to expand range by each level
     difficulty = 5
 
     # Choose a secret value
     secret = randint(1, values)
-
+    
+    #option to turn on or off hints after each level
+    hints_prompt = input(f'Would you like to activate hints? Hints will tell you if the number you guessed is too high or low. [y/n]').lower()
+    if hints_prompt == "n":
+      print("Hints are disabled, good luck!")
+      hints=False
+    if hints_prompt == "y":
+      hint_count+=1
+      print("Hints are enabled.")
+      hints=True
+    else:
+      print("Ok... I'll just take that as a no.")
+      hints=False
+        
     # Guess loop
     while lives > 0:
         print(f"Lives remaining: {lives}\n")
         try:
+ 
             guess = int(input(f"I'm thinking of a number between 1 and {values}, what is it? "))
             if guess not in range(1, values+1):
                 raise ValueError
@@ -31,16 +47,38 @@ while True:
             print("Please guess a number in the appropriate range.")
 
         if guess == secret:
+
             level += 1
             lives += 1
             values += difficulty
+            
+            #Found a flaw in your game, if you leveled up, the "secret" number stayed exactly the same, so I added this to change the number everytime you level up.
+            secret= randint(1, values)
+            
             print(f"You got it! Moving on to level {level}. Here's an extra life to help you along.")
+            
+            #Dialogue for winning the game/beating level 5
+            if level==6:
+              if hint_count==0:
+                
+                #no hints easter egg
+                print("Woah there, you beat level 5 using no hints at all? You must either have the luck of the gods or be a cheater. Well, idk, so here, have a cookie.")
 
+              win_prompt=input(f"Wow, you officially won the game and beat level 5, congrats! Would you like to continue to infinite level mode? [yes/quit]")
+              if win_prompt=="quit":
+                break
         else:
             print("Nope, lose a life!")
             lives -= 1
+            
+            if hints==True:
+                if guess>secret:
+                  print("The number you guessed was too high")
+                if guess<secret:
+                  print("The number you guessed was too low")
 
     print("GAME OVER\n")
-    again = input("Play again? [Y]/n:").lower()
+    print(f"The number I was thinking of was {secret}.")
+    again = input("Play again? [y/n]:").lower()
     if again == "n":
         break
